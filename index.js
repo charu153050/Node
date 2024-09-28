e6a5ba56049469d333c3df7fadebd020f13dc0ca
charu153050 <charukukreja15@gmail.com>
2024-09-28 15:11:06 +0530

    Express

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url")
+// const http = require("http");
 
-const myServer = http.createServer((req, res) => {
-    if(req.url === "/favicon.ico") return res.end()
-  const log = `${Date.now()} ${req.url}:New Request Recieved\n`;
-const myUrl = url.parse(req.url , true)
-console.log(myUrl)
-  fs.appendFile("log.txt",log,(err,data)=>{
-    switch(myUrl.pathname){
-        case "/": res.end("Hello from server");
-        break
-        case "/about":
-            const username = myUrl.query.name
-             res.end(`Hello from ${username}`); 
-        break
-        default : res.end("404 Page Not Found"); 
-    }
-
-  })
-})
-  
-
-
-
-myServer.listen(8000, () => {
-  console.log("Server is started");
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  return res.end("Hello from HomePage");
 });
+app.get("/about", (req, res) => {
+  return res.end("Hello from About Page" + "Hey" + req.query.name);
+});
+
+// const myServer = http.createServer(app);
+
+// myServer.listen(8000, () => {
+//   console.log("Server is started");
+// });
+app.listen(8000,()=>{
+    console.log("Server Started")
+})
